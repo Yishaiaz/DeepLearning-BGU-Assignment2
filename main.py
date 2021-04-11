cfae75943cf2d0a7965371ec83fa5e38c45fcf3c
Liad Giladi <liad.giladi@algosec.com>
2021-04-11 23:08:26 +0300

    enriched siamese network

--- main.py
+++ main.py
@@ -5,39 +5,42 @@ import shutil
 import tensorflow as tf
 from tensorboard.plugins.hparams import api as hp
 
+from siamese_network import SiameseNeuralNetwork
 from utils import file_path, log
 
 
 def configure_hyper_param_grid():
     HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['sgd']))
-    HP_LEARNING_RATE = hp.HParam('learning_rate', hp.Discrete([0.001, 0.0001]))
-    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([32, 128, 512]))
-    HP_NUM_UNITS1 = hp.HParam('num_units_1', hp.Discrete([24]))
+    HP_LEARNING_RATE = hp.HParam('learning_rate', hp.Discrete([0.1, 0.01, 0.001, 0.0001]))
+    HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([32, 64, 128, 512]))
+    HP_DENSE_UNITS_NUM = hp.HParam('dense_units_num', hp.Discrete([4096, 2048, 1024]))
 
-    return HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_NUM_UNITS1
+    return HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_DENSE_UNITS_NUM
+
 
 def run(experiment_num,
         experiment_name,
         current_time,
+        input_shape,
         hparams,
         batch_size,
         optimizer,
         learning_rate,
-        num_units1,
+        dense_units_num,
         logger):
 
     logger.info("\nRun experiment {}:\n{}\n".format(experiment_num, {h.name: hparams[h] for h in hparams}))
 
-    tf_writer = tf.summary.create_file_writer(file_path("/tf-logs/") + f"hparam_tuning/{current_time}/{experiment_num}")
+    tf_writer = tf.summary.create_file_writer(file_path("/tf-logs/") + f"hparam_tuning/{current_time}/{experiment_num}/{experiment_name}")
 
     with tf_writer.as_default():
         hp.hparams(hparams)
 
-        siamese_network = SiameseNeuralNetwork()
+        siamese_network = SiameseNeuralNetwork(input_shape)
         siamese_network.train()
 
         # log metrics
-        tf.summary.scalar()
+        # tf.summary.scalar()
 
         # save model
         model_dir_name = f"{current_time}_{experiment_num}_{experiment_name}"
@@ -53,24 +56,24 @@ def run_experiments():
     if os.path.isdir(tf_logs_dir):
         shutil.rmtree(tf_logs_dir)
 
-
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
     # configure hyper-parameters grid-search
-    HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_NUM_UNITS1 = configure_hyper_param_grid()
+    HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_DENSE_UNITS_NUM = configure_hyper_param_grid()
 
     # metrics
-    metric_accuracy = "validation accuracy"
+    metric_oneshot_accuracy = "validation oneshot_accuracy"
 
     with tf.summary.create_file_writer(tf_logs_dir + "/hparam_tuning").as_default():
         hp.hparams_config(
-            hparams=[HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_NUM_UNITS1],
-            metrics=[hp.Metric(metric_accuracy, display_name=metric_accuracy)],
+            hparams=[HP_OPTIMIZER, HP_LEARNING_RATE, HP_BATCH_SIZE, HP_DENSE_UNITS_NUM],
+            metrics=[hp.Metric(metric_oneshot_accuracy, display_name=metric_oneshot_accuracy)],
         )
 
     experiment_num = 0
+    lfw_a_input_shape = (250, 250, 1)
 
-    for num_units1 in HP_NUM_UNITS1.domain.values:
+    for dense_units_num in HP_DENSE_UNITS_NUM.domain.values:
         for batch_size in HP_BATCH_SIZE.domain.values:
             for learning_rate in HP_LEARNING_RATE.domain.values:
                 for optimizer in HP_OPTIMIZER.domain.values:
@@ -78,17 +81,17 @@ def run_experiments():
                         HP_OPTIMIZER: optimizer,
                         HP_LEARNING_RATE: learning_rate,
                         HP_BATCH_SIZE: batch_size,
-                        HP_NUM_UNITS1: num_units1,
+                        HP_DENSE_UNITS_NUM: dense_units_num,
                     }
 
                     experiment_num += 1
                     experiment_name = "batch_size={}," \
                                       "optimizer={}," \
                                       "learning_rate={}," \
-                                      "num_units1={}".format(batch_size,
-                                                             optimizer,
-                                                             learning_rate,
-                                                             num_units1)
+                                      "dense_units_num={}".format(batch_size,
+                                                                  optimizer,
+                                                                  learning_rate,
+                                                                  dense_units_num)
 
                     # init experiments logger
                     log_filename = file_path(f"experiments/{current_time}/{experiment_num}/{experiment_name}.txt")
@@ -100,12 +103,14 @@ def run_experiments():
                     logger.info("#################################################################")
 
                     run(experiment_num,
+                        experiment_name,
                         current_time,
+                        lfw_a_input_shape,
                         hparams,
                         batch_size,
                         optimizer,
                         learning_rate,
-                        num_units1,
+                        dense_units_num,
                         logger)
 
 
--- siamese_network.py
+++ siamese_network.py
@@ -1,39 +1,42 @@
+import tensorflow as tf
+
 from functools import partial
 from logging import Logger
 from typing import Tuple
 
 from tensorflow.python.keras import Input, Sequential, Model, regularizers
-from tensorflow.python.keras.layers import Lambda, Dense, Conv2D, BatchNormalization
+from tensorflow.python.keras.callbacks import LearningRateScheduler
+from tensorflow.python.keras.layers import Lambda, Dense, Conv2D, BatchNormalization, MaxPooling2D, Dropout, Flatten
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
 from tensorflow.python.keras.optimizer_v2.rmsprop import RMSprop
 from tensorflow.keras import backend as keras_backend
+from tensorflow.python.ops.init_ops_v2 import RandomNormal
 
 from utils import default_log
 
 
-class SiameseNeuralNetwork():
+class SiameseNeuralNetwork:
     """
-    This class is a Siamese neural network implementation based on # TODO
+    This class is a Siamese neural network implementation based on paper
+    'Siamese Neural Networks for One-shot Image Recognition'.
     """
-    __standard_layers = (Conv2D(64, (10, 10), activation='relu'),
-                         MaxPooling2D(),
-                         Conv2D(128, (7, 7), activation='relu'),
-                         MaxPooling2D(),
-                         Conv2D(128, (4, 4), activation='relu'),
-                         MaxPooling2D(),
-                         Conv2D(256, (4, 4), activation='relu'),
-                         Flatten(),
-                         Dense(4096, activation='sigmoid'))
-
     def __init__(self,
                  input_shape: Tuple[int, int, int],
-                 layers: Tuple[Any] = __standard_layers,
-                 learning_rate: float = 0.00006,
-                 batch_size: int = 32,
+                 learning_rate: float = 0.01,
+                 batch_size: int = 128,
                  optimizer: str = 'sgd',
                  optimizer_loss: str = 'binary_crossentropy',
+                 dense_layer_size: int = 4096,
+                 enable_batch_normalization: bool = False,
+                 l2_regularizer: float = None,
+                 bias_initializer: str = None,
+                 conv2D_kernel_initializer: str = None,
+                 dense_kernel_initializer: str = None,
+                 distance_metric: str = None,
+                 dropout_rate: float = None,
+                 enable_learning_rate_decay_scheduler: bool = True,
                  logger: Logger = None,
                  verbose: int = 0):
 
@@ -48,50 +51,85 @@ class SiameseNeuralNetwork():
 
         self._seed = None
 
-        # build model
-        self._model = self._build_model()
-        self._compile(self._model)
+        self._dense_layer_size = dense_layer_size
+        self._enable_batch_normalization = enable_batch_normalization
+        self._l2_regularizer = l2_regularizer
+        self._bias_initializer = bias_initializer
+        self._conv2D_kernel_initializer = conv2D_kernel_initializer
+        self._dense_kernel_initializer = dense_kernel_initializer
+        self._distance_metric = distance_metric
+        self._dropout_rate = dropout_rate
+        self._enable_learning_rate_decay_scheduler = enable_learning_rate_decay_scheduler
+
+        # build model and compile
+        self.model = self._build_model()
+        self._compile()
 
         self._trained = False
 
-
-    def _build_model(self):
+    def _build_model(self) -> Model:
+        """
+        Build the underlying model based on given hyperparameters
+        :return: model
+        """
         input_shape = self._input_shape
 
         first_twin_network = Input(input_shape)
         second_twin_network = Input(input_shape)
 
-        RegularizedConv2D = partial(Conv2D,
-                                    activation="relu",
-                                    kernel_initializer="glorot_normal",
-                                    kernel_regularizer=regularizers.l2(0.01))
+        conv2D_kernel_initializer = self._conv2D_kernel_initializer if self._conv2D_kernel_initializer is not None else RandomNormal(
+            mean=0., stddev=10**-2)
+        dense_kernel_initializer = self._dense_kernel_initializer if self._dense_kernel_initializer is not None else RandomNormal(
+            mean=0., stddev=2 * (10**-1))
+        bias_initializer = self._bias_initializer if self._bias_initializer is not None else RandomNormal(mean=0.5,
+                                                                                                          stddev=10**-2)
+
+        conv2d_layer = partial(Conv2D,
+                               activation="relu",
+                               kernel_initializer=conv2D_kernel_initializer,
+                               bias_initializer=bias_initializer,
+                               kernel_regularizer=regularizers.l2(
+                                   self._l2_regularizer) if self._l2_regularizer is not None else None)
+
+        dense_layer = partial(Dense,
+                              activation='sigmoid',
+                              kernel_initializer=dense_kernel_initializer,
+                              bias_initializer=bias_initializer,
+                              kernel_regularizer=regularizers.l2(
+                                  self._l2_regularizer) if self._l2_regularizer is not None else None)
+
+        def _construct_conv2d_layer(_model, filters, kernel_size):
+            _model.add(conv2d_layer(filters, kernel_size))
+            if self._enable_batch_normalization:
+                _model.add(BatchNormalization())
+            _model.add(MaxPooling2D())
+            if self._dropout_rate is not None:
+                _model.add(Dropout(rate=self._dropout_rate))
 
         model = Sequential()
-        model.add(Conv2D(64, (10, 10), activation='relu', input_shape=input_shape))
-        if self._enable_batch_normalization:
-            model.add(BatchNormalization())
-        model.add(model.MaxPooling2D())
+        _construct_conv2d_layer(model, 64, (10, 10))
+        _construct_conv2d_layer(model, 128, (7, 7))
+        _construct_conv2d_layer(model, 128, (4, 4))
+        _construct_conv2d_layer(model, 256, (4, 4))
 
-        # Conv2D(128, (7, 7), activation='relu'),
-        # MaxPooling2D(),
-        # Conv2D(128, (4, 4), activation='relu'),
-        # MaxPooling2D(),
-        # Conv2D(256, (4, 4), activation='relu'),
-        # Flatten(),
-        # Dense(4096, activation='sigmoid')
-        # # add all layers including the embedding
-        # for layer in layers:
-        #     model.add(layer)
+        model.add(Flatten())
+
+        model.add(dense_layer(self._dense_layer_size))
 
         first_embedding = model(first_twin_network)
         second_embedding = model(second_twin_network)
 
         # calculate the distance between twins with lambda layer
-        lambda_layer = Lambda(lambda embedding_vectors: keras_backend.abs(embedding_vectors[0] - embedding_vectors[1]))
+        if self._distance_metric is None or self._distance_metric == "abs":
+            lambda_layer = Lambda(lambda embedding_vectors: keras_backend.abs(embedding_vectors[0] - embedding_vectors[1]))
+        else:
+            lambda_layer = Lambda(lambda embedding_vectors:
+                                  keras_backend.sqrt(keras_backend.sum(keras_backend.square(embedding_vectors[0] - embedding_vectors[1]), axis=1, keepdims=True)))
+
         twins_dist = lambda_layer((first_embedding, second_embedding))
 
         # define output layer, classifying whether it is the same person in the image
-        layer_output = Dense(1, activation='sigmoid')(twins_dist)
+        layer_output = dense_layer(1)(twins_dist)
 
         model = Model(inputs=(first_twin_network, second_twin_network), outputs=layer_output)
 
@@ -101,14 +139,26 @@ class SiameseNeuralNetwork():
         return model
 
     def _compile(self):
-        if self._optimizer == 'sgd':
-            opt = SGD(learning_rate=self._learning_rate)
-        elif self._optimizer == 'adam':
-            opt = Adam(learning_rate=self._learning_rate)
-        else:
-            opt = RMSprop(learning_rate=self._learning_rate)
+        """
+        Compile the model with an optimizer and callbacks such as learning rate decay
+        :return: None
+        """
+        def learning_rate_decay_callback(epoch, lr):
+            return tf.math.scalar_mul(0.99, lr)
 
-        self._model.compile(optimizer=opt, loss=self._optimizer_loss)
+        callbacks = []
+
+        if self._enable_learning_rate_decay_scheduler:
+            callbacks.append(LearningRateScheduler(learning_rate_decay_callback))
+
+        if self._optimizer == 'sgd':
+            opt = SGD(learning_rate=self._learning_rate, callbacks=callbacks, momentum=0.5)
+        elif self._optimizer == 'adam':
+            opt = Adam(learning_rate=self._learning_rate, callbacks=callbacks)
+        else:
+            opt = RMSprop(learning_rate=self._learning_rate, callbacks=callbacks)
+
+        self.model.compile(optimizer=opt, loss=self._optimizer_loss)
 
     def summary(self) -> None:
         """
@@ -123,8 +173,7 @@ class SiameseNeuralNetwork():
         :param model_dir_path: directory path instruct where to save the model
         :return: None
         """
-        self._model.save(model_dir_path + "/model.h5")
-
+        self.model.save(model_dir_path + "/model.h5")
 
     def load(self, model_dir_path: str):
         """
@@ -132,19 +181,56 @@ class SiameseNeuralNetwork():
         :param model_dir_path: directory path instruct from where to load the model
         :return: None
         """
-        self._model = load_model(model_dir_path + "/model.h5")
+        self.model = load_model(model_dir_path + "/model.h5")
         self._trained = True
 
-    def train(self):
+    def train(self,
+              train_ds,
+              val_ds,
+              max_epoch_num: int = 200,
+              patience: int = 20,
+              seed: int = None):
         if self._trained:
             raise ValueError("Network was already trained")
 
-        # handle seed TODO
+        # handle seed
+        if seed is not None:
+            tf.random.set_seed(seed)
+
+        history = self.model.fit(train_ds, epochs=max_epoch_num, val_ds=val_ds, batch_size=128)
+
+        # for epoch in range(max_epoch_num):
+        #     pass
+        #
+        #
+        #     # save best model during training
+        #
+        #     # early stopping
+
 
         self._trained = True
 
-    def predict(self):
-        if not self._trained:
+    def test_n_way(self, dataset, num_tests: int, n: int = 20, is_validation: bool = False):
+        """
+        TODO
+        :param dataset:
+        :param n:
+        :param is_validation:
+        :return:
+        """
+        if not self._trained and not is_validation:
             raise ValueError("Network wasn't trained")
 
-        pass
+        num_correct = tf.constant(0)
+
+        for test_idx in range(num_tests):
+            # generate one-shot learning N-way accuracy test
+            input_pairs, labels = generate_n_way_oneshot_accuracy_tests(dataset)
+
+            preds_tensor = self.model.predict(input_pairs)
+            preds_argmax_idx = tf.math.argmax(preds_tensor, axis=1)
+
+            if tf.math.equal(preds_argmax_idx, tf.constant(0)):
+                num_correct += 1
+
+        return (num_correct/num_tests) * 100
